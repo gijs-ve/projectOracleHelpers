8070560e696e2e15621f15e68539ca007135f3fe
Gijsbert van Everdingen <super-gijs@hotmail.com>
2024-09-28 17:45:18 +0200

    fix: added name to register api

--- lib/apiClient/auth.ts
+++ lib/apiClient/auth.ts
@@ -2,7 +2,13 @@ import { fetchApi } from './fetch';
 
 const route = '/auth';
 export const getAuthClient = (serverUrl: string) => {
-    const login = async (email: string, password: string) => {
+    const login = async ({
+        email,
+        password,
+    }: {
+        email: string;
+        password: string;
+    }) => {
         const response = await fetchApi(serverUrl, `${route}/login`, {
             method: 'POST',
             body: JSON.stringify({ email, password }),
@@ -16,10 +22,18 @@ export const getAuthClient = (serverUrl: string) => {
         return response.data;
     };
 
-    const register = async (email: string, password: string) => {
+    const register = async ({
+        name,
+        email,
+        password,
+    }: {
+        name: string;
+        email: string;
+        password: string;
+    }) => {
         const response = await fetchApi(serverUrl, `${route}/register`, {
             method: 'POST',
-            body: JSON.stringify({ email, password }),
+            body: JSON.stringify({ name, email, password }),
             headers: {
                 'Content-Type': 'application/json',
             },
